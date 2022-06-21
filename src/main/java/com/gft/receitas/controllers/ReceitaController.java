a95cd1ab63abd860742c44abc24aa09a8026eeec
geag <guilherme.camargo@gft.com>
2022-06-21 15:33:55 -0300

    Criando estética das páginas e seu layout

--- src/main/java/com/gft/receitas/controllers/ReceitaController.java
+++ src/main/java/com/gft/receitas/controllers/ReceitaController.java
@@ -1,8 +1,71 @@
 package com.gft.receitas.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gft.receitas.entities.Receita;
+import com.gft.receitas.services.ReceitaService;
 
 @Controller
+@RequestMapping("/receita")
 public class ReceitaController {
 
-}
+	@Autowired
+	private ReceitaService receitaService;
+	
+	@RequestMapping(path = "/manage")
+	public ModelAndView editarReceita(@RequestParam (required=false) Long id) {
+		ModelAndView mv = new ModelAndView("receita/form");
+		Receita receita;
+		
+		if(id==null) {
+			receita = new Receita();
+		} else {
+			try {
+				receita = receitaService.obterReceita(id);
+			} catch (Exception e) {
+				receita = new Receita();
+				mv.addObject("mensagem", e.getMessage());
+			}
+		}
+		
+		mv.addObject("receita", receita);
+		
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, path = "/manage")
+	public ModelAndView salvarReceita(@Valid Receita receita, BindingResult bindingResult) {
+		ModelAndView mv = new ModelAndView("receita/form");
+		
+		boolean novo = true;
+		
+		if(receita.getId() != null) {
+			mv.addObject("receita", receita);
+			novo = false;
+		}
+			
+		if(bindingResult.hasErrors()) {
+			mv.addObject("receita", receita);
+		}
+		
+		receitaService.salvarReceita(receita);
+		
+		if(novo) {
+			mv.addObject("receita", new Receita());
+		} else {
+			mv.addObject("receita", receita);
+		}
+
+		mv.addObject("mensagem", "Receita salva com sucesso!");
+		
+		return mv;
+	}
+ }
--- src/main/java/com/gft/receitas/services/UnidadeMedidaService.java
+++ src/main/java/com/gft/receitas/services/UnidadeMedidaService.java
@@ -1,11 +1,41 @@
 package com.gft.receitas.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gft.receitas.entities.UnidadeMedida;
 import com.gft.receitas.repositories.UnidadeMedidaRepository;
 
 @Service
 public class UnidadeMedidaService {
 	
+	@Autowired
 	private UnidadeMedidaRepository unidadeMedidaRepository;
+	
+	
+	public UnidadeMedida salvarUnidadeMedida (UnidadeMedida unidadeMedida) {
+		return unidadeMedidaRepository.save(unidadeMedida);
+	}
+	
+	public UnidadeMedida obterUnidadeMedida (Long id) throws Exception{
+		Optional<UnidadeMedida> unidadeMedida = unidadeMedidaRepository.findById(id);
+		
+		if (unidadeMedida.isEmpty()) {
+			throw new Exception ("Unidade de Medida não encontrada!");
+		}
+		
+		return unidadeMedida.get();
+	}
+	
+	public List<UnidadeMedida> listaUnidadeMedida () {
+		return unidadeMedidaRepository.findAll();
+	}
+	
+	public void excluirUnidadeMedida(Long id) {
+		unidadeMedidaRepository.deleteById(id);
+	}
+	
 }
